433e9010db9c0fb207c7518db0ad6aebc7f417a6
NavyaTatavarty <tsrinavya@gmail.com>
2016-02-24 14:52:56 +0530

    hello

---
+++ singlefile/nav4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int sort1(int arr[],int x)
+ {
+   int temp,i,j;
+   for(i=0;i<=x;i++)
+    {
+      for(j=0;j<=(x-i-1);j++)
+       {
+         if(arr[j]>arr[j+1])
+           {
+             temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+           }
+         }
+      }
+printf("The sorted elements are:\n");
+     for(i=0;i<=x;i++)
+      printf("%d",arr[i]);
+     return 0;
+ }
+
+extern int sort1();
+int array[20];
+ int main()
+  {
+    int n,i;
+    printf("Enter the size of the array");
+    scanf("%d",&n);
+    printf("Enter the array elements:\n");
+    for(i=0;i<n;i++)
+     {
+       scanf("%d",&array[i]);
+     }
+    sort1  (array,n);
+    return 0;
+  }
+ 
